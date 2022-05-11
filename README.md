62e95d477f1d151bbaecbf976ce6db1e8fef6141
M. A <mak@nyantec.com>
2022-05-11 11:27:22 +0200

    Pin nixpkgs in shell.nix

--- README.md
+++ README.md
@@ -23,6 +23,15 @@ X-SSL-Client-Dn header. With nginx your configuration might look like this:
 ...
 ```
 
+## Developing with nix-shell
+
+`nix-shell` is a tool to start an interactive shell based on a Nix expression.
+The provided `shell.nix` contains code to start an interactive shell with thei
+necessary dependencies to build this project.
+
+To spawn the shell, simply invoke `nix-shell` from this directory.
+
+
 ## License
 ```
 Copyright © 2021-2022 nyantec GmbH <oss@nyantec.com>
--- shell.nix
+++ shell.nix
@@ -1,8 +1,12 @@
-with import <nixpkgs> {};
-
-mkShell {
+let
+  # Pinned version of pkgs
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/08dc90729fc8b4ab072607cf7257900a9cacb1f6.tar.gz")) {};
+  # Uncomment if you want to try a more recent nixpkgs checkout instead
+  #pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
+in
+pkgs.mkShell {
   name = "snipeit-cert-auth-dev-shell";
-  buildInputs = [
+  buildInputs = with pkgs; [
     cargo rustc
     openssl
     pkg-config
